617bf6ac274c5053e8f947486c5184a668676e62
Marouane Rassili <mrassili@aol.com>
2019-01-29 17:51:14 +0100

    Improve getting started form

--- src/components/GetStartedForm.js
+++ src/components/GetStartedForm.js
@@ -19,29 +19,57 @@ class GetStartedForm extends React.Component {
         <div className={styles.container}>
           <h1>Please fill in the informations below to get started!</h1>
           <form className={styles.form}>
-            <label htmlFor="fullName">
-              Full Name :
-              <input type="text" id="fullName" />
+            <label htmlFor="fullName" className={styles.entryLabel}>
+              Full Name :{/* eslint-disable-next-line */}
+              <input type="text" id="fullName" autoFocus />
             </label>
             {/* eslint-disable-next-line */}
-            <label htmlFor="genre">
+            <label htmlFor="genre" className={styles.entryLabel}>
               Genre :
               <select id="genre">
                 <option>Male</option>
                 <option>Female</option>
               </select>
             </label>
-            <label htmlFor="age">
+            <label htmlFor="age" className={styles.entryLabel}>
               Age :
               <input type="number" placeholder="e.g. 20" min="18" />
             </label>
-            <label htmlFor="height">
+            <label htmlFor="height" className={styles.entryLabel}>
               Height :
               <input type="number" id="height" placeholder="e.g. 150" min="0" />
+              <div className={styles.unitToggle}>
+                <fieldset className={styles.radioSwitch}>
+                  <legend>Height Unit</legend>
+                  <input type="radio" name="height" id="ft" />
+                  <label htmlFor="ft" className="switchLabel">
+                    Feet
+                  </label>
+
+                  <input type="radio" name="height" id="cm" />
+                  <label htmlFor="cm" className="switchLabel">
+                    Cm
+                  </label>
+                </fieldset>
+              </div>
             </label>
-            <label htmlFor="weight">
+            <label htmlFor="weight" className={styles.entryLabel}>
               Weight :
               <input type="number" id="weight" placeholder="e.g. 160" min="0" />
+              <div className={styles.unitToggle}>
+                <fieldset className={styles.radioSwitch}>
+                  <legend>Height Unit</legend>
+                  <input type="radio" name="weight" id="lbs" />
+                  <label htmlFor="lbs" className="switchLabel">
+                    Lbs
+                  </label>
+
+                  <input type="radio" name="weight" id="kg" />
+                  <label htmlFor="kg" className="switchLabel">
+                    Kg
+                  </label>
+                </fieldset>
+              </div>
             </label>
           </form>
         </div>
